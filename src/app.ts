d149bf3f9c3eaaf2b2b986770d3916e17b3e9cb9
Taesik yoon <taesikyoon97@naver.com>
2022-10-20 23:02:09 +0900

    Add gitignore

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("hello world!!");
+import * as express  from 'express';
+const app: express.Express = express();
+const port = 8000;
+
+
+app.get(`/`, (req:express.Request,res:express.Response)=>{
+  console.log(req);
+  res.send({name:`taesikyoon97`,age:`26`,friends:[`유`,`상`,`민`]});
+});
+
+app.listen(port,()=>{
+  console.log(`Server Runing http://localhost:${port}`);
+});
